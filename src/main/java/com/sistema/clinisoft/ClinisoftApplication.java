a8b9d462240e8fe9f33999fb2c8ee8b50c080748
Jean-Vidal <jeancvidal2020@gmail.com>
2025-01-25 22:15:13 -0300

    Inicia configuracoes projeto

---
+++ src/main/java/com/sistema/clinisoft/ClinisoftApplication.java
@@ -0,0 +1,13 @@
+package com.sistema.clinisoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinisoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinisoftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sistema/clinisoft/ClinisoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistema.clinisoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinisoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
